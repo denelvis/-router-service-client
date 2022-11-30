2531076462632a1f679282e6a96c5a654ece30a7
Denis <denelvis@yandex.ru>
2022-11-30 22:13:33 +0300

    feat: add findVideo method

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ export class RouterClientService {
 
   public async registerVideos(videos: IVideoFile[]) {
     const wsClient = this.wss;
-    const routes: string[] = [];
+    const routes: Set<string> = new Set();
 
     wsClient.on("open", () => {
       wsClient.send(JSON.stringify({ id: routerClientId, files: videos }));
@@ -23,7 +23,23 @@ export class RouterClientService {
       wsClient.onmessage = (msg) => {
         const routeStrings: string[] = JSON.parse(msg.data.toString());
         if (Object.keys(routeStrings).includes("routerServersUrls")) {
-          routes.push(...routeStrings);
+          routes.add(JSON.stringify(routeStrings));
+        }
+      };
+    });
+  }
+
+  public async findVideo(videoId: string) {
+    const wsClient = this.wss;
+    const holderFileServiceUrls: string[] = [];
+
+    wsClient.on("open", () => {
+      wsClient.send(JSON.stringify({ find: videoId }));
+
+      wsClient.onmessage = (msg) => {
+        const holder: string[] = JSON.parse(msg.data.toString());
+        if (Object.keys(holder).includes("holderFileServiceUrls")) {
+          holderFileServiceUrls.push(...holder);
         }
       };
     });
