cb53bb61d4ee7037f15517bd1d5be91d7ee5174c
Denis <denelvis@yandex.ru>
2022-12-04 21:36:29 +0300

    fix: add return to class methods

--- src/index.ts
+++ src/index.ts
@@ -13,33 +13,35 @@ export class RouterClientService {
     this.wss = new WebSocket(url);
   }
 
-  public async registerVideos(videos: IVideoFile[]) {
+  public async registerVideos(videos: IVideoFile[]): Promise<string[] | void> {
     const wsClient = this.wss;
-    const routes: Set<string> = new Set();
 
     wsClient.on("open", () => {
       wsClient.send(JSON.stringify({ id: routerClientId, files: videos }));
 
       wsClient.onmessage = (msg) => {
-        const routeStrings: string[] = JSON.parse(msg.data.toString());
+        const routeStrings: Record<string, string[]> = JSON.parse(
+          msg.data.toString()
+        );
         if (Object.keys(routeStrings).includes("routerServersUrls")) {
-          routes.add(JSON.stringify(routeStrings));
+          return routeStrings.routerServersUrls;
         }
       };
     });
   }
 
-  public async findVideo(videoId: string) {
+  public async findVideo(videoId: string): Promise<string[] | void> {
     const wsClient = this.wss;
-    const holderFileServiceUrls: string[] = [];
 
     wsClient.on("open", () => {
       wsClient.send(JSON.stringify({ find: videoId }));
 
       wsClient.onmessage = (msg) => {
-        const holder: string[] = JSON.parse(msg.data.toString());
+        const holder: Record<string, string[]> = JSON.parse(
+          msg.data.toString()
+        );
         if (Object.keys(holder).includes("holderFileServiceUrls")) {
-          holderFileServiceUrls.push(...holder);
+          return holder.holderFileServiceUrls;
         }
       };
     });
