7906d08482d2f34b792d85e0f5bafd7083f87bcb
Denis <denelvis@yandex.ru>
2022-12-07 21:29:07 +0300

    feat: add webpack with config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+// webpack.config.js
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+  mode: "production",
+  entry: {
+    routerClientService: path.resolve(__dirname, "./src/index.ts"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].min.js",
+    library: "libraryStarter",
+    libraryTarget: "umd",
+    globalObject: "this",
+  },
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.(j|t)s$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+  externals: [
+    {
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+    },
+  ],
+  externalsPresets: { node: true },
+  resolve: {
+    extensions: [".js", ".ts"],
+    symlinks: false,
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      assert: require.resolve("assert"),
+      buffer: require.resolve("buffer"),
+      util: require.resolve("util"),
+      url: require.resolve("url"),
+      https: require.resolve("https-browserify"),
+      http: require.resolve("stream-http"),
+      stream: require.resolve("stream-browserify"),
+      zlib: require.resolve("browserify-zlib"),
+    },
+  },
+};
